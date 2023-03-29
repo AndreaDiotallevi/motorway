3d0fcbe6c3661025bad9f80c4e732e9d07c6258b
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2023-03-29 17:29:16 +0100

    add id primary key to stateLogs"

--- src/db/scripts/dump.sql
+++ src/db/scripts/dump.sql
@@ -15,14 +15,17 @@ INSERT INTO "vehicles" (id, make, model, state) VALUES (3, 'VW',   'GOLF', 'sold
 
 -- Create StateLogs Table
 CREATE TABLE IF NOT EXISTS "stateLogs" (
+  "id"  INTEGER NOT NULL,
   "vehicleId"  INTEGER NOT NULL,
   "state" TEXT NOT NULL,
-  "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL
+  "timestamp" TIMESTAMP WITH TIME ZONE NOT NULL,
+
+  PRIMARY KEY ("id")
 );
 
-INSERT INTO "stateLogs" ("vehicleId", state, timestamp) VALUES (1, 'quoted',  '2022-09-10 10:23:54+00');
-INSERT INTO "stateLogs" ("vehicleId", state, timestamp) VALUES (2, 'quoted',  '2022-09-10 14:59:01+00');
-INSERT INTO "stateLogs" ("vehicleId", state, timestamp) VALUES (2, 'selling', '2022-09-11 17:03:17+00');
-INSERT INTO "stateLogs" ("vehicleId", state, timestamp) VALUES (3, 'quoted',  '2022-09-11 09:11:45+00');
-INSERT INTO "stateLogs" ("vehicleId", state, timestamp) VALUES (3, 'selling', '2022-09-11 23:21:38+00');
-INSERT INTO "stateLogs" ("vehicleId", state, timestamp) VALUES (3, 'sold',    '2022-09-12 12:41:41+00');
+INSERT INTO "stateLogs" (id, "vehicleId", state, timestamp) VALUES (1, 1, 'quoted',  '2022-09-10 10:23:54+00');
+INSERT INTO "stateLogs" (id, "vehicleId", state, timestamp) VALUES (2, 2, 'quoted',  '2022-09-10 14:59:01+00');
+INSERT INTO "stateLogs" (id, "vehicleId", state, timestamp) VALUES (3, 2, 'selling', '2022-09-11 17:03:17+00');
+INSERT INTO "stateLogs" (id, "vehicleId", state, timestamp) VALUES (4, 3, 'quoted',  '2022-09-11 09:11:45+00');
+INSERT INTO "stateLogs" (id, "vehicleId", state, timestamp) VALUES (5, 3, 'selling', '2022-09-11 23:21:38+00');
+INSERT INTO "stateLogs" (id, "vehicleId", state, timestamp) VALUES (6, 3, 'sold',    '2022-09-12 12:41:41+00');
