a82120bde64f7459bdf97a58d4098b89441634ca
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2023-03-29 17:30:00 +0100

    export state logs files

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import vehiclesRouter from "./vehicles"
+import stateLogsRouter from "./stateLogs"
 
 const router = Router()
 
 router.use("/vehicles", vehiclesRouter)
+router.use("/stateLogs", stateLogsRouter)
 
 export default router
