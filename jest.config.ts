fda9d4cac9ef6903bc73a901d1af9a8e13f5f906
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2023-03-30 21:16:52 +0100

    setup jest testing

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,11 @@ import type { Config } from "@jest/types"
 
 // Sync object
 const config: Config.InitialOptions = {
-    globals: {
-        "ts-jest": {
-            tsconfig: "./tsconfig.json",
-        },
-    },
-    preset: "ts-jest",
     rootDir: "src",
     verbose: true,
+    transform: {
+        "^.+\\.ts?$": ["ts-jest", { tsconfig: "./tsconfig.json" }],
+    },
 }
 
 export default config
---
+++ src/services/vehicles.test.ts
@@ -0,0 +1,6 @@
+import { app } from "../app"
+
+it("test", async () => {
+    const test = "fd"
+    expect(2).toEqual(2)
+})
