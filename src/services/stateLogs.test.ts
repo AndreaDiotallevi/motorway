7e4f4bf99b418c53c8570ab6a4a58c6f6cb32657
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2023-03-31 14:16:03 +0100

    write test for successful path

--- src/services/stateLogs.test.ts
+++ src/services/stateLogs.test.ts
@@ -1,29 +1,28 @@
-import { app } from "../app"
 import { StateLogAttributes } from "../models"
 import { getStateLogForTimestamp } from "./stateLogs"
 
-it("test", async () => {
-    const stateLogs: StateLogAttributes[] = [
-        {
-            id: 1,
-            vehicleId: 3,
-            state: "quoted",
-            timestamp: "2022-09-11 09:11:45+00",
-        },
-        {
-            id: 2,
-            vehicleId: 3,
-            state: "selling",
-            timestamp: "2022-09-11 23:21:38+00",
-        },
-        {
-            id: 3,
-            vehicleId: 3,
-            state: "sold",
-            timestamp: "2022-09-12 12:41:41+00",
-        },
-    ]
+const stateLogs: StateLogAttributes[] = [
+    {
+        id: 1,
+        vehicleId: 3,
+        state: "quoted",
+        timestamp: "2022-09-11 09:11:45+00",
+    },
+    {
+        id: 2,
+        vehicleId: 3,
+        state: "selling",
+        timestamp: "2022-09-11 23:21:38+00",
+    },
+    {
+        id: 3,
+        vehicleId: 3,
+        state: "sold",
+        timestamp: "2022-09-12 12:41:41+00",
+    },
+]
 
+it("should return the state log the vehicle was at the given timestamp", async () => {
     const vehicle = getStateLogForTimestamp({ stateLogs, timestamp: "2022-09-12 10:00:00+00" })
 
     expect(vehicle?.state).toEqual("selling")
