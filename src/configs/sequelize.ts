d55864ea4d9c065cc57a03c026143ab9f2ee8f1c
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2023-03-31 14:11:06 +0100

    define sequelize config for each environment

--- src/configs/sequelize.ts
+++ src/configs/sequelize.ts
@@ -1,15 +1,56 @@
-import { Dialect, Sequelize } from "sequelize"
+import { Dialect, Options, Sequelize } from "sequelize"
 
-const dbName = process.env.DB_NAME as string
-const dbUser = process.env.DB_USER as string
-const dbHost = process.env.DB_HOST as string
-const dbDialect = process.env.DB_DIALECT as Dialect
-const dbPassword = process.env.DB_PASSWORD
+enum NodeEnvironment {
+    Development = "development",
+    Test = "test",
+    Production = "production",
+}
 
-const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
-    port: 5432,
-    host: dbHost,
-    dialect: dbDialect,
-})
+const config: Record<
+    NodeEnvironment,
+    {
+        name: string
+        user: string
+        password: string
+        options: Options
+    }
+> = {
+    development: {
+        name: process.env.DB_NAME as string,
+        user: process.env.DB_USER as string,
+        password: process.env.DB_PASSWORD as string,
+        options: {
+            port: Number(process.env.DB_PORT),
+            host: process.env.DB_HOST as string,
+            dialect: process.env.DB_DIALECT as Dialect,
+        },
+    },
+    test: {
+        name: "motorway",
+        user: "user",
+        password: "password",
+        options: {
+            port: 5432,
+            host: "db-test",
+            dialect: "postgres",
+        },
+    },
+    production: {
+        name: process.env.DB_NAME as string,
+        user: process.env.DB_USER as string,
+        password: process.env.DB_PASSWORD as string,
+        options: {
+            port: Number(process.env.DB_PORT),
+            host: process.env.DB_HOST as string,
+            dialect: process.env.DB_DIALECT as Dialect,
+        },
+    },
+}
+
+const nodeEnv = process.env.NODE_ENV as NodeEnvironment
+console.log(nodeEnv)
+
+const { name, user, password, options } = config[nodeEnv]
+const sequelize = new Sequelize(name, user, password, options)
 
 export default sequelize
